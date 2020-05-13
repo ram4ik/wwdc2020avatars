55fb264dc4b24d700639ffb3029ac79588f8d992
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-13 22:52:56 +0300

    Initial Commit

---
+++ wwdc2020avatars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wwdc2020avatars
+//
+//  Created by Ramill Ibragimov on 13.05.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
